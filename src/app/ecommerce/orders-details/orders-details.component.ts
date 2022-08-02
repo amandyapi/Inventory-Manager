7cfd5a3ec99dbaf386b5d156571a3be824903d1a
Amand <yapiamand@gmail.com>
2022-08-02 13:25:17 +0000

    orders

--- src/app/ecommerce/orders-details/orders-details.component.ts
+++ src/app/ecommerce/orders-details/orders-details.component.ts
@@ -2,6 +2,8 @@ import { OrderService } from './../../shared/services/order.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { StorageService } from './../../shared/services/storage.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-orders-details',
@@ -11,16 +13,26 @@ import { StorageService } from './../../shared/services/storage.service';
 export class OrdersDetailsComponent implements OnInit {
 
   order: any;
+  closeResult = '';
 
   constructor(
     private orderService: OrderService,
     private storageService: StorageService,
     private router: Router,
+    private modalService: NgbModal
   ) { }
 
   ngOnInit(): void {
     this.order = this.storageService.getItem('orderDetails');
     console.log('orderDetails', this.order);
+  }
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed`;
+    });
   }
 
 }
